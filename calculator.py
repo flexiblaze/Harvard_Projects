9ac304d7aee2ef86ccfe78344c5cb4c019cf5501
flexiblaze <ilikcay24@gmail.com>
2025-10-06 11:28:35 +0200

    second harvard projects
    first lesson done
    
    extra projects done

---
+++ calculator.py
@@ -0,0 +1,7 @@
+
+#make them integer with int
+
+x = int(input("What is x? "))
+y = int(input("What is y? "))
+
+print(x+y)
---
+++ hello.py
@@ -0,0 +1,47 @@
+
+#Asking user for their name
+
+name = input("Wat is jouw naam? ").strip().title()
+
+#remove the whitespace from string str.
+name = name.strip().title().capitalize()
+name = name.capitalize() #capitalize user's  only first letter
+name = name.title() # hepsi buyuk yapar
+
+#split user's name into first name and last name
+
+first, last = name.split(" ")
+
+
+#i can also use + or , here
+print("Hello World",first,"Bond"  )
+print(f"Hello World, {first}") # format string, f string is uniek
+#function =>print
+
+#arguments input to a function => Hello World
+
+#side effects => uitslag van function
+
+#bug's mistake in a program code
+
+#return values , go get that values,put it somewhere then if you want it print it
+
+#variables .can store a value like a container right to left
+
+#comments not for my self
+#pseudocode = expressing my thoughts on how to do it
+"""
+Anything here is also comment
+"""
+
+#str => string type of data, \n new line
+# print(*objects, sep='',end ='\n', file=sys.stdout, flush=False)
+#sep means seperater tussen twee  words, end means u can add new line or something after the first variable
+
+#parameteres =>  value in functions
+
+print('Hello, "friend"')
+print("Hello, 'friend'")
+print('Hello, \"friend\"') # escaping
+
+#int => numbers , no decimals
